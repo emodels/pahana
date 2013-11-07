3ce1843fea64d61c4f652a3dd46711703f8d2a14
danesh <danesh@danesh-PC>
2013-11-07 23:12:58 +0530

    Admin control development completed

--- protected/views/site/paper.php
+++ protected/views/site/paper.php
@@ -1,6 +1,12 @@
 <?php $this->pageTitle=Yii::app()->name . ' - Paper';?>
 <script type="text/javascript">
-$(document).ready(function(){ 
+$(document).ready(function(){
+    $('#lnk_next').click(function(){
+        $('#divScroller').scrollLeft($('#divScroller').scrollLeft() + 920);
+    });
+    $('#lnk_prev').click(function(){
+        $('#divScroller').scrollLeft($('#divScroller').scrollLeft() - 920);
+    });
     $(window).scroll(function(){
         if ($(this).scrollTop() > 100) {
             $('.scrollup').fadeIn();
@@ -24,7 +30,7 @@ function LoadPage(strFile){
 <section id="content">
     <div class="wrapper-after" style="padding: 10px; position: relative">
         <h1 style="font-size: 22px; padding: 10px 0 10px 0"><?php echo ($model->type == 0 ? 'Monthly Issue' : 'Mid Monthly Issue') . ' : ' . date("F", mktime(0, 0, 0, $model->month, 10)) . ' - ' . $model->year; ?></h1>
-        <div style="width: 100%; overflow-x: scroll; overflow-y: hidden; border: solid 1px silver;">
+        <div id="divScroller" style="width: 100%; overflow-x: scroll; overflow-y: hidden; border: solid 1px silver;">
             <div class="row" style="padding-top: 10px; width: <?php echo 230 * $model->page_count; ?>px; height: 290px;">
                 <?php
                 $this->widget('zii.widgets.CListView', array(
@@ -34,7 +40,9 @@ function LoadPage(strFile){
                 ));
                 ?>
             </div>
-        </div>    
+        </div>
+        <div style="position: absolute; top: 135px; right: -20px"><a id="lnk_next" href="javascript:return false;"><img src="<?php echo Yii::app()->baseUrl; ?>/images/right_arrow.png" class="hover_opacity"/></a></div>
+        <div style="position: absolute; top: 135px; left: -20px"><a id="lnk_prev" href="javascript:return false;"><img src="<?php echo Yii::app()->baseUrl; ?>/images/left_arrow.png" class="hover_opacity"/></a></div>
         <div>
             <iframe id="frmPage" src="<?php echo Yii::app()->baseUrl . '/' . $dataProvider->data[0]->folder . $dataProvider->data[0]->name; ?>" style="width: 100%; height: 1320px"></iframe>
         </div>
