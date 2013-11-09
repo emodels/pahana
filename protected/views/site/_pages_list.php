bcb4d575c110f71d6c8efe5bf5de02c8c21328fc
danesh <danesh@danesh-PC>
2013-11-09 16:10:19 +0530

    Development phase completed

--- protected/views/site/_pages_list.php
+++ protected/views/site/_pages_list.php
@@ -3,7 +3,7 @@
         <div style="position: absolute; width: 200px; height: 260px; border: solid 2px black; left: 8px"></div>
     </a>    
     <div style="text-align: center; color: navy; font-weight: bold; display: inline-block">
-    <iframe src="<?php echo Yii::app()->baseUrl . '/' . $data->folder . $data->name; ?>" style="width: 200px; height: 262px;"></iframe><br/>
+    <iframe src="<?php echo Yii::app()->baseUrl . '/' . $data->folder . $data->name; ?>#toolbar=0&navpanes=0" scrolling="no" frameborder="0" style="width: 200px; height: 262px; overflow: hidden;" marginheight="0" marginwidth="0"></iframe><br/>
     <a href="javascript:LoadPage('<?php echo Yii::app()->baseUrl . '/' . $data->folder . $data->name; ?>');">Page - <?php echo str_replace('.pdf', '', $data->name); ?></a>
     </div>
 </div>
